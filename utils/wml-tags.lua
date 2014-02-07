d7abffd73e94dedf63268f1857e845a0d6b904c2
trewe <sjrs456@yandex.com>
2014-02-07 00:59:16 +0000

    several small fixed, spotted by backporting it

--- utils/wml-tags.lua
+++ utils/wml-tags.lua
@@ -15,7 +15,7 @@ local helper = wesnoth.require "lua/helper.lua"
 
 function wml_actions.store_shroud(cfg)
 	local team_number = cfg.side or helper.wml_error("Missing required side= attribute in [store_shroud]")
-	local variable = cfg.variable or helper.wml_error("Missing required variable= attribute in [store_shroud].")
+	local variable = cfg.variable or "shroud_data"
 	local side = wesnoth.sides[team_number]
 	local current_shroud = side.__cfg.shroud_data
 	wesnoth.set_variable(variable, current_shroud)
