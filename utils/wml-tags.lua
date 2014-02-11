69e8ff96e724a4ebc28dea69885bf4ff5ba5bf19
trewe <sjrs456@yandex.com>
2014-02-11 18:35:07 +0000

    removed unnecessary local variables

--- utils/wml-tags.lua
+++ utils/wml-tags.lua
@@ -16,9 +16,7 @@ local helper = wesnoth.require "lua/helper.lua"
 function wml_actions.store_shroud(cfg)
 	local team_number = cfg.side or helper.wml_error("Missing required side= attribute in [store_shroud]")
 	local variable = cfg.variable or "shroud_data"
-	local side = wesnoth.sides[team_number]
-	local current_shroud = side.__cfg.shroud_data
-	wesnoth.set_variable(variable, current_shroud)
+	wesnoth.set_variable(variable, wesnoth.sides[team_number].__cfg.shroud_data)
 end
 
 --! [set_shroud]
